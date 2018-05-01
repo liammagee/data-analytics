8cf4bc86942edc3a65493204964615167ff68e51
Ubuntu <ubuntu@ics-live-instance.novalocal>
2018-05-01 07:33:31 +0000

    Update CoS with Hobart data

--- common.py
+++ common.py
@@ -9,11 +9,12 @@ import seaborn as sns
 
 
 class DataSet:
-    def __init__(self, filename):
+    def __init__(self, filename = None):
         self.data = {}
         self.metadata = {}
-        self.load_spss_files(filename)
-        self.assign_columns()
+        if filename != None:
+            self.load_spss_files(filename)
+            self.assign_columns()
 
     # Functions
     def load_spss_files(self, filename):
@@ -32,21 +33,32 @@ class DataSet:
         return dict([(k, v.decode('windows-1252')) for k, v in self.metadata.valueLabels[bytes(var, encoding='windows-1252')].items()])
 
     def freq_table(self, var):
-        return pd.DataFrame(list(gself.et_value_labels(var).items()), columns=['index', self.get_var_label(var)]) \
+        return pd.DataFrame(list(self.get_value_labels(var).items()), columns=['index', self.get_var_label(var)]) \
                 .set_index('index', drop=True) \
                 .merge(pd.DataFrame(self.data[var].value_counts().sort_index()) \
                        .rename(columns = {var: 'Frequency'}), \
                        left_index = True, right_index = True) \
 
 
-    def gen_histogram(self, col):
+    def gen_histogram(self, col, legend_labels = None):
         fig, ax = plt.subplots()
         labels = self.get_value_labels(col).values()
         bin_length = len(labels)
-        n, bins, patches = ax.hist(self.data[col].dropna(), bins = np.arange(1, bin_length + 2) - 0.5, rwidth = 0.8)
+        
+        # If legend labels exist, split the data by the first index value (assumes len(legend_labels) == len(self.data.index.levels))
+        if legend_labels != None and hasattr(self.data.index, 'levels') and len(legend_labels) == len(self.data.index.levels):
+            d = pd.Series(self.data['An_1'], index = self.data.index).unstack(level=0).as_matrix().T
+            d = [x[~np.isnan(x)] for x in d]
+        else:
+            d = self.data[col].dropna()
+        n, bins, patches = ax.hist(d, bins = np.arange(1, bin_length + 2) - 0.5, rwidth = 0.8, stacked=True, label = legend_labels)
 
         # Add grid
         ax.grid(True)
+        
+        # Legend
+        if legend_labels != None:
+            ax.legend(prop={'size': 10})
 
         bin_centers = 0.5 * (bins[:-1] + bins[1:])
         # scale values to interval [0,1]
@@ -60,8 +72,8 @@ class DataSet:
 
         # Apply colours
         cm = plt.get_cmap('Blues')
-        for c, patch in zip(bns, patches):
-            patch.set_facecolor(cm(c))
+        #for c, patch in zip(bns, patches):
+        #    patch.set_facecolor(cm(c))
 
         # Add title and axes
         plt.title(self.get_var_label(col), loc='left')
@@ -73,5 +85,13 @@ class DataSet:
         # Tweak spacing to prevent clipping of ylabel
     #     fig.tight_layout()
         plt.show()
+        
+        import tabulate
+        from IPython.display import HTML, display
+        df = pd.DataFrame(np.asarray(n).T, index=labels)
+        if legend_labels == None:
+            legend_labels = ['Frequencies'] 
+        s = tabulate.tabulate(df, headers=legend_labels, tablefmt='html')
+        display(HTML(s))
 
 
